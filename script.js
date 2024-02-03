2f52a83c4c6fe6e1d58d62094489d044e9d8711c
Tom Evans <35412739+TomE1@users.noreply.github.com>
2024-02-03 10:01:30 +0700

    3rd time lucky?

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-// script for tab links
-
-var tabLinks = document.getElementsByClassName("tab-links");
-var tabContents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-  for (tabLink of tabLinks) {
-    tabLink.classList.remove("active-link");
-  }
-  for (tabContent of tabContents) {
-    tabContent.classList.remove("active-tab");
-  }
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
-}
-
 // script for menu
-var sidemenu = document.getElementById("sidemenu");
+var sideMenu = document.getElementById("sideMenu");
 
-function openmenu() {
-  sidemenu.style.right = "0";
+function openMenu() {
+  sideMenu.style.right = "0";
 }
-function closemenu() {
-  sidemenu.style.right = "-200px";
+function closeMenu() {
+  sideMenu.style.right = "-200px";
 }
